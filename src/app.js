929b980718a3f580716c55278e30c3fb0540f744
Kenneth Oh <ohtkenneth@gmail.com>
2018-09-19 12:30:23 -0700

    Configure for AWS EC2
    Add public directory to hold index.html and add refactor file send.

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(express.static(path.join(__dirname, '../dist')));
 
 app.get('/', (req, res) => {
   console.log(req.url);
-  res.sendFile(path.join(__dirname, '../dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 module.exports = app;
